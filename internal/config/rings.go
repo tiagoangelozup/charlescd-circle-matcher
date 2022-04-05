e6969e6bbd8dca1b48f07afa042895cd4b67ab49
Tiago Angelo <tiago.angelo@zup.com.br>
2022-04-05 13:45:26 -0300

    Unmarshall configurations

--- internal/config/rings.go
+++ internal/config/rings.go
@@ -3,12 +3,14 @@ package config
 import (
 	"fmt"
 	"github.com/buger/jsonparser"
+	"github.com/tiagoangelozup/charlescd-circle-matcher/internal/log"
 )
 
+const pathNotFoundErrMsg = "Key path not found"
+
 type (
-	converter func([]byte, jsonparser.ValueType, int, error)
-	Rings     []*Ring
-	Ring      struct {
+	Rings []*Ring
+	Ring  struct {
 		ID    string
 		Match Match
 	}
@@ -19,41 +21,119 @@ type (
 	Rule struct {
 		Key      string
 		Operator string
-		Values   []interface{}
+		Values   []*RuleValue
 	}
+	RuleValue struct {
+		value    []byte
+		dataType jsonparser.ValueType
+	}
+	builder struct {
+		rings Rings
+	}
+	converter func([]byte, jsonparser.ValueType, int, error)
 )
 
-type builder struct {
-	rings Rings
+func (r *RuleValue) IsNumber() (float64, bool, error) {
+	if r.dataType != jsonparser.Number {
+		return 0, false, nil
+	}
+	val, err := jsonparser.ParseFloat(r.value)
+	if err != nil {
+		return 0, true, fmt.Errorf("error unmarshalling rule value %q: %v", string(r.value), err)
+	}
+	return val, true, nil
 }
 
-func NewRings(raw PluginRawData) Rings {
+func (r *Rule) NumberValues() []float64 {
+	results := make([]float64, 0)
+	for _, value := range r.Values {
+		if val, ok, err := value.IsNumber(); err != nil {
+			log.Warnf("%v", err)
+		} else if ok {
+			results = append(results, val)
+		}
+	}
+	return results
+}
+
+func NewRings(raw PluginRawData) (Rings, error) {
 	b := &builder{rings: make(Rings, 0)}
-	jsonparser.ArrayEach(raw, funcName(b), "rings")
-	return b.rings
+	_, err := jsonparser.ArrayEach(raw, extractRing(b), "rings")
+	if err != nil && err.Error() != pathNotFoundErrMsg {
+		return nil, fmt.Errorf("error unmarshalling rings from plugin configuration: %w", err)
+	}
+	log.Debugf("%d rings have been configured", len(b.rings))
+	return b.rings, nil
 }
 
-func funcName(b *builder) converter {
+func extractRing(b *builder) converter {
 	return func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 		ring := new(Ring)
-		ring.ID, _ = jsonparser.GetString(value, "id")
-		jsonparser.ArrayEach(value, funcName2(ring), "match", "any")
+		ring.ID, err = jsonparser.GetString(value, "id")
+		if err != nil {
+			log.Warnf("failed to extract the ring ID from %q: %v", string(value), err)
+			return
+		}
+		_, err = jsonparser.ArrayEach(value, extractAllRules(ring), "match", "all")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rules from %q: %v", string(value), err)
+			return
+		}
+		_, err = jsonparser.ArrayEach(value, extractAnyRules(ring), "match", "any")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rules from %q: %v", string(value), err)
+			return
+		}
 		b.rings = append(b.rings, ring)
 	}
 }
 
-func funcName2(r *Ring) converter {
+func extractAllRules(r *Ring) converter {
 	return func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 		rule := new(Rule)
-		rule.Key, _ = jsonparser.GetString(value, "key")
-		rule.Operator, _ = jsonparser.GetString(value, "operator")
-		jsonparser.ArrayEach(value, funcName3(rule), "values")
+		rule.Key, err = jsonparser.GetString(value, "key")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rule key from %q: %v", string(value), err)
+			return
+		}
+		rule.Operator, err = jsonparser.GetString(value, "operator")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rule operator from %q: %v", string(value), err)
+			return
+		}
+		_, err = jsonparser.ArrayEach(value, extractValues(rule), "values")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rule values from %q: %v", string(value), err)
+			return
+		}
+		r.Match.All = append(r.Match.All, rule)
+	}
+}
+
+func extractAnyRules(r *Ring) converter {
+	return func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		rule := new(Rule)
+		rule.Key, err = jsonparser.GetString(value, "key")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rule key from %q: %v", string(value), err)
+			return
+		}
+		rule.Operator, err = jsonparser.GetString(value, "operator")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rule operator from %q: %v", string(value), err)
+			return
+		}
+		_, err = jsonparser.ArrayEach(value, extractValues(rule), "values")
+		if err != nil && err.Error() != pathNotFoundErrMsg {
+			log.Warnf("failed to extract rule values from %q: %v", string(value), err)
+			return
+		}
 		r.Match.Any = append(r.Match.Any, rule)
 	}
 }
 
-func funcName3(rule *Rule) converter {
+func extractValues(rule *Rule) converter {
 	return func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		fmt.Println(value)
+		rule.Values = append(rule.Values, &RuleValue{value: value, dataType: dataType})
 	}
 }
--- internal/config/rings_test.go
+++ internal/config/rings_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestNewRings(t *testing.T) {
 	j := `{"rings":[{"id":"9d22edc0-db79-412e-9e4d-d420ec5826d0","match":{"any":[{"key":"request.auth.claims.age","operator":"GreaterThan","values":[30,31]}]}},{"id":"cbb548ce-e412-4b16-9191-e06544beb69d","match":{"all":[{"key":"request.auth.claims.city","operator":"Equals","values":["São Carlos/SP",33]}]}}]}`
-	rings := NewRings([]byte(j))
-	require.Len(t, rings, 1)
+	rings, err := NewRings([]byte(j))
+	require.NoError(t, err)
+	require.Len(t, rings, 2)
 }
